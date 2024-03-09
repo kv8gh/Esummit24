f7a2d5ba7c5f29275aea53469639906e25d8d260
kv8gh <vyaskaran1409@gmail.com>
2024-03-09 23:07:16 +0530

    adding pages of event 3,4,5,6

--- app/api/event2/makeTeam/route.js
+++ app/api/event2/makeTeam/route.js
@@ -2,48 +2,52 @@ import { connectMongoDB } from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 import { UsersDetails } from "@/models/Userdetails";
 import { Event2 } from "@/models/event2.model";
+import { Users } from "@/models/user";
 
-import {getTokenDetails} from "../../../../utils/authuser"
+import { getTokenDetails } from "../../../../utils/authuser";
+import { generateTokens } from "../../login/generateTokensTeam/route";
 
-export async function POST(req){
-    try{
-        await connectMongoDB();
-        const headers = req.headers;
-        //console.log(headers.get("authorization"))
+export async function POST(req) {
+  try {
+    await connectMongoDB();
+    const headers = req.headers;
 
-      
+    const auth = req.headers.get("authorization").split(" ")[1];
 
-        const auth = req.headers.get("authorization").split(' ')[1];
-        //console.log(auth)
-        let userId = await getTokenDetails(auth);
-        //console.log(userId)
-        const user = await Users.findById({ _id: userId});
-        console.log(user);
+    let userId = await getTokenDetails(auth);
+    console.log(userId);
 
-        const {teamName}=await req.json();
-        const team=await Event2.findOne({teamName:teamName})
-        if(team){
-            return NextResponse.json({ message: "Team Already registered ", status: 200 });
-        }
-        console.log(teamName)
-        const newTeam = await new Event2({
-        teamName:teamName
-        
-    
+    const user = await UsersDetails.findById({ _id: userId });
+
+    console.log(user);
+
+    const { teamName } = await req.json();
+    const team = await Event2.findOne({ teamName: teamName });
+    if (team) {
+      return NextResponse.json({
+        message: "Team Already registered ",
+        status: 200,
+      });
+    }
+    // console.log(teamName)
+    const newTeam = await new Event2({
+      teamName: teamName,
+      teamLeaderId: userId,
+      members: [userId],
     }).save();
 
-    await Users.updateMany(
-        { _id: userId },
-        { $set: { teamId: newTeam._id } }
+    await UsersDetails.findByIdAndUpdate(
+      { _id: userId },
+      { $set: { teamId: newTeam._id } }
     );
- 
+
+    const { accessToken, refreshToken } = await generateTokens(newTeam);
+    //console.log(accessToken);
+    //console.log(refreshToken);
+
     return NextResponse.json({ message: "Team Details entered ", status: 200 });
-
-
-
-
-}catch(error) {
+  } catch (error) {
     console.error("An error occurred:", error);
     return NextResponse.json({ message: "Error occurred ", status: 500 });
-}
+  }
 }
