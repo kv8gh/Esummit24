8d488cf8bd39da126a98d89200be12c590ae7a3c
arjunbector <arjunbector@gmail.com>
2024-03-14 14:53:07 +0530

    add deregister router for event 3, 4, 5

---
+++ app/api/event5/deregister/route.js
@@ -0,0 +1,44 @@
+import { Users } from "@/models/user";
+import { connectMongoDB } from "@/lib/mongodb";
+import { getTokenDetails } from "@/utils/authuser";
+import { NextResponse } from "next/server";
+
+export async function GET(req, _) {
+  try {
+    await connectMongoDB();
+    const auth = req.headers.get("authorization").split(" ")[1];
+    const userId = await getTokenDetails(auth);
+    if (!userId) {
+      return NextResponse.json({ message: "User not found." }, { status: 404 });
+    }
+
+    const user = await Users.findById(userId);
+    if (!user) {
+      return NextResponse.json({ message: "User not found." }, { status: 404 });
+    }
+    const currentEventsArr = user.events;
+    if (!currentEventsArr.includes(5)) {
+      return NextResponse(
+        { message: "User has not registered for event5 previously." },
+        { status: 200 }
+      );
+    }
+    for (let i = 0; i < currentEventsArr.length; i++) {
+      if (currentEventsArr[i] === 5) {
+        currentEventsArr.splice(i, 1);
+      }
+    }
+    user.events = currentEventsArr;
+    await user.save();
+    return NextResponse.json(
+      { message: "User deregistered for event5 successfully." },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { message: `Internal Server Error. ${err.message}` },
+      { status: 500 }
+    );
+  }
+}
