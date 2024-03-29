8deef8d438b6d75ab3158b1edcc55685d6b2584b
Isha <ishushree19@gmail.com>
2024-03-29 15:46:20 +0530

    about section added

---
+++ components/landingPage/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    // <div className="text-center shadow-md rounded-lg p-8 text-white border-gradient-to-b from-blue-500 to-purple-500 border-2 mx-24">
+    //     <h2 className="text-4xl font-black mb-2">About E-Summit'24</h2>
+    //     <p className="text-gray-700">
+    //         E-Summit is the conclave that opens the doors to the world of
+    //         entrepreneurship. Masquerading as a single event, it is a fusion of
+    //         visionary activities and insightful sessions that open the realm of
+    //         networking and broaden the horizon of business in students' minds. With
+    //         its exclusive class of exhilarating events, interesting workshops,
+    //         exciting expos and spectacular speakers, E-Summit aims at instilling
+    //         innovation in young minds.
+    //     </p>
+
+    // </div>
+    <div class="mx-auto flex min-h-screen max-w-screen-sm items-center justify-center">
+      <div class="h-full w-full rounded-md bg-gradient-to-b from-[#ffffff1d] via-[#fefab733] to-[#ffd18c52] p-1 border-2 border-[#D6993F]">
+        <div class=" h-full w-full items-center justify-center bg-gray-800 back p-8 text-center text-white bg-transparent">
+          <h1 class="text-4xl font-black p-4">About E-Summit'24</h1>
+          <p className="">
+            E-Summit is the conclave that opens the doors to the world of
+            entrepreneurship. Masquerading as a single event, it is a fusion of
+            visionary activities and insightful sessions that open the realm of
+            networking and broaden the horizon of business in students' minds.
+            With its exclusive class of exhilarating events, interesting
+            workshops, exciting expos and spectacular speakers, E-Summit aims at
+            instilling innovation in young minds.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
