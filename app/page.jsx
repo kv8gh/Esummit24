8168b735f32631ada678d0133aa3efce2f0db8a4
Arjun Bector <arjunbector@gmail.com>
2024-03-31 01:21:31 +0530

    add onComplete to timer (#56)

--- app/page.jsx
+++ app/page.jsx
@@ -14,10 +14,12 @@ import FAQ from "@/components/landingPage/FAQ/FAQ";
 import { Footer } from "@/components/landingPage/Footer/Footer";
 import About from "@/components/landingPage/About";
 import Who from "@/components/landingPage/Who";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Home() {
   const scheduleRef = useRef(null);
+  const [regOpen, setRegOpen] = useState(true);
+
   const { status, data: session } = useSession();
 
   const router = useRouter();
@@ -39,7 +41,53 @@ export default function Home() {
   // }
   return (
     <>
-      <HeroSection scheduleRef={scheduleRef}/>
+      {/* <div>
+        <div className="grid grid-cols-2">
+          <Link
+            href="/events/event1"
+            className="m-3 w-1/4 bg-slate-500 border border-black p-5 text-white"
+          >
+            {" "}
+            go to Event 1
+          </Link>
+          <Link
+            href="/events/event2"
+            className="m-3 w-1/4 bg-slate-500 border border-black p-5 text-white"
+          >
+            {" "}
+            go to Event 2
+          </Link>
+          <Link
+            href="/events/event3"
+            className="m-3 w-1/4 bg-slate-500 border border-black p-5 text-white"
+          >
+            {" "}
+            go to Event 3
+          </Link>
+          <Link
+            href="/events/event4"
+            className="m-3 w-1/4 bg-slate-500 border border-black p-5 text-white"
+          >
+            {" "}
+            go to Event 4
+          </Link>
+          <Link
+            href="/events/event5"
+            className="m-3 w-1/4 bg-slate-500 border border-black p-5 text-white"
+          >
+            {" "}
+            go to Event 5
+          </Link>
+          <Link
+            href="/events/event6"
+            className="m-3 w-1/4 bg-slate-500 border border-black p-5 text-white"
+          >
+            {" "}
+            go to Event 6
+          </Link>
+        </div>
+      </div> */}
+      <HeroSection scheduleRef={scheduleRef} regOpen={regOpen} setRegOpen={setRegOpen}/>
       <IIA />
       <About />
       <Who />
