84eafecb4b4e1c4d4e837ba5e6034fadacea45fa
kv8gh <vyaskaran1409@gmail.com>
2024-03-30 15:01:52 +0530

    userDetails page

--- app/userDetails/page.jsx
+++ app/userDetails/page.jsx
@@ -3,7 +3,10 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import Image from "next/image";
+import eSummit from "@/public/assets/logos/esummitLogo.svg";
 import toast, { Toaster } from "react-hot-toast";
+import Navbar from "@/components/Navbar";
 
 export default function UserDetails() {
   const { data: session, status } = useSession();
@@ -40,8 +43,6 @@ export default function UserDetails() {
   const handleRegNo = (e) => {
     const inputValue = e.target.value;
     setRegNo(inputValue.toUpperCase());
-    
-    
   };
 
   const handleSubmit = async () => {
@@ -49,8 +50,8 @@ export default function UserDetails() {
     console.log(firstName);
     console.log(lastName);
 
-    const regNoNew = document.getElementById('regNo').value;
-    const isValidInput = (/^[2][0-3][a-zA-Z]{3}\d{4}$/).test(regNoNew.trim());
+    const regNoNew = document.getElementById("regNo").value;
+    const isValidInput = /^[2][0-3][a-zA-Z]{3}\d{4}$/.test(regNoNew.trim());
     console.log(isValidInput);
     if (isValidInput) {
       setRegError(false);
@@ -59,7 +60,6 @@ export default function UserDetails() {
       setRegError(true);
     }
 
-    
     if (mobNo !== "" && regNo !== "") {
       if (mobNo.length === 10) {
         if (isValidInput && regNo.length === 9) {
@@ -111,49 +111,116 @@ export default function UserDetails() {
   };
 
   return (
-    <div className="h-[100vh] flex flex-col items-center gap-6">
-      {/* <div>
-        <label htmlFor="firstName">First Name:</label>
-        <input
-          type="text"
-          id="firstName"
-          value={firstName}
-          onChange={handleFirstnameChange}
-          style={{ color: "black" }}
-        />
+    <main className="min-w-[100vw] min-h-[100vh] flex justify-center items-center bg-black">
+    <Navbar/>
+      {/* {isLoading && <LoadingScreen/>} */}
+      <div className="flex flex-col md:flex-row w-full h-[80vh] md:h-[90vh] justify-evenly items-center">
+        <div
+          className="hidden md:w-100 h-5/6 md:flex flex-col justify-center px-4 pb-5 pt-3 rounded-3xl border-solid border-4 border-[linear-gradient(126deg, #DCA64E 16.34%, #FEFAB7 53.16%, #D6993F 100%)]"
+          style={{ backgroundColor: "black" }}
+        >
+          <Image
+            src={eSummit}
+            alt="eSummit-Logo"
+            className="h-2/3 w-2/3 self-center"
+          />
+          <div className="text-white text-6xl font-bold flex flex-col items-center">
+            E-SUMMIT&apos;24
+          </div>
+        </div>
+
+        <div
+          className="w-4/5 md:w-1/2 h-5/6 flex flex-col justify-evenly md:justify-around items-start rounded-3xl px-4 border-solid border-4 border-[linear-gradient(126deg, #DCA64E 16.34%, #FEFAB7 53.16%, #D6993F 100%)]"
+
+          style={{ backgroundColor: "black" }}
+        >
+          <div className="flex justify-start items-center">
+            <h1 className="text-3xl text-white font-bold">
+              Enter Your Information
+            </h1>
+          </div>
+
+          <form id="registrationForm" className="shadow-md rounded w-full">
+            <div className="mb-4">
+              <label
+                className="block text-white text-lg font-bold font-poppins"
+                htmlFor="name"
+              >
+                First Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-3/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="name"
+                type="text"
+                placeholder="First Name"
+                value={firstName}
+                onChange={handleFirstnameChange}
+              ></input>
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-white text-lg font-bold  font-poppins"
+                htmlFor="name"
+              >
+                Last Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-3/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="name"
+                type="text"
+                placeholder="Last Name"
+                value={lastName}
+                onChange={handleLastnameChange}
+              ></input>
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-white text-lg font-bold "
+                htmlFor="regNo"
+              >
+                Registration Number
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-3/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="regNo"
+                type="text"
+                placeholder="Registration Number"
+                value={regNo}
+                onChange={(e) => {
+                  // setRegNo(e.target.value);
+                  handleRegNo(e);
+                }}
+              ></input>
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-white text-lg font-bold "
+                htmlFor="phone"
+              >
+                Phone Number
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-3/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="phone"
+                type="tel"
+                placeholder="Phone Number"
+                value={mobNo}
+                onChange={handlePhoneNumber}
+              ></input>
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="text-black bg-gradient-to-br from-[#DCA64E] via-[#FEFAB7] to-[#D6993F] hover:bg-gradient-to-br hover:from-amber-400 hover:via-amber-200 hover:to-yellow-400 focus:outline-none focus:ring-cyan-800 dark:focus:ring-cyan-300 font-medium rounded-3xl text-lg px-5 py-2 text-center me-2 mb-2"
+              >
+                Register
+              </button>
+              <Toaster />
+            </div>
+          </form>
+        </div>
       </div>
-      <div>
-        <label htmlFor="lastName">Last Name:</label>
-        <input
-          type="text"
-          id="lastName"
-          value={lastName}
-          onChange={handleLastnameChange}
-          style={{ color: "black" }}
-        />
-      </div> */}
-      <div>
-        <label htmlFor="regNo">Registration Number:</label>
-        <input
-          type="text"
-          id="regNo"
-          value={regNo}
-          onChange={handleRegNo}
-          style={{ color: "black" }}
-        />
-      </div>
-      <div>
-        <label htmlFor="mobNo">Mobile Number:</label>
-        <input
-          type="text"
-          id="mobNo"
-          value={mobNo}
-          onChange={handlePhoneNumber}
-          style={{ color: "black" }}
-        />
-      </div>
-      <button onClick={() => handleSubmit()} className="bg-blue border border-black rounded-lg p-3">Submit</button>
-      <Toaster />
-    </div>
+    </main>
   );
 }
