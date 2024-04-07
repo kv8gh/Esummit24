f984199af5ffc62e34f5ee4af9506ac72a7b6bf9
arjunbector <arjunbector@gmail.com>
2024-04-08 01:00:33 +0530

    fix: build error

--- app/mySchedule/page.jsx
+++ app/mySchedule/page.jsx
@@ -4,58 +4,12 @@ import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import ScheduleRegisterButton from "@/components/events/ScheduleRegisterButton";
 import Loader from "@/components/Loader";
-import { FaLocationDot } from "react-icons/fa6";
-import { FaRegClock } from "react-icons/fa";
+import Event from "./Event";
 
-function Event({ event }) {
-  const [loader, setLoader] = useState(false);
-  const { data: session, status } = useSession();
+// const page = () =>
 
-  return (
-    <div>
-      {loader && <Loader />}
-      <div className="">
-        <h1 className="uppercase text-2xl md:text-3xl lg:text-5xl font-bold bg-gradient-to-br from-[#DCA64E] via-[#FEFAB7] to-[#D6993F] bg-clip-text text-transparent">
-          {event.eventName}
-        </h1>
-        <div className="flex gap-2 items-center">
-          <span>
-            <FaRegClock />
-          </span>
-          {event.time}
-        </div>
-        <div className="flex gap-2 items-center">
-          <span>
-            <FaLocationDot />
-          </span>
-          {event.venue}
-        </div>
-      </div>
-      <p className="font-poppins py-2">{event.description}</p>
-      <div className="flex gap-2">
-        <ScheduleRegisterButton
-          loader={loader}
-          setLoader={setLoader}
-          event={event.id}
-          token={session?.accessTokenBackend}
-        />
-        {(event.id === 1 || event.id === 2) && (
-          <button
-            className="text-black font-semibold hover:scale-105 transition-all bg-gradient-to-br from-[#DCA64E] via-[#FEFAB7] to-[#D6993F] p-2 rounded-lg hover:bg-opacity-80"
-            onClick={() => {
-              window.location.href = `/events/event${event.id}/memberDash`;
-            }}
-          >
-            Go to Dashboard
-          </button>
-        )}
-      </div>
-    </div>
-  );
-}
-
-const page = () => {
-  const [regEvent, setRegEvents] = useState([1, 2, 3, 4]);
+const MySchedule = () => {
+  const [regEvent, setRegEvents] = useState([]);
   const [regEventsList, setRegEventsList] = useState([]);
   const [userDetails, setUserDeatials] = useState(null);
   const [loader, setLoader] = useState(true);
@@ -81,12 +35,11 @@ const page = () => {
           )
         );
         setLoader(false);
-        // console.log(data);
       });
   }, []);
 
   const events = regEventsList.map((event) => {
-    return <Event event={event} />;
+    return <Event key={event.id} event={event} />;
   });
 
   return (
@@ -119,4 +72,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default MySchedule;
