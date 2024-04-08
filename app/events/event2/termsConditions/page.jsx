5ecee69374704a8f0351a37c41c0b341d90e3846
kv8gh <vyaskaran1409@gmail.com>
2024-04-08 09:43:43 +0530

    adding loader in consent page

--- app/events/event2/termsConditions/page.jsx
+++ app/events/event2/termsConditions/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Loader from "@/components/Loader";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -7,8 +8,10 @@ export default function TermsConditions() {
   const router = useRouter();
   const { data: session, status } = useSession();
   const [check, setCheck] = useState();
+  const [isLoading,setIsLoading] = useState(false);
 
   const getData = () => {
+    setIsLoading(true);
     fetch(`api/userDetails`, {
       content: "application/json",
       method: "GET",
@@ -22,6 +25,7 @@ export default function TermsConditions() {
       .then((data) => {
         const user = data.user;
         setCheck(user.event2Consent);
+        setIsLoading(false);
 
         // if (user.hasFilledDetails == true) {
         //   if (user.teamId !== null) {
@@ -35,6 +39,7 @@ export default function TermsConditions() {
   };
 
   function consent() {
+    setIsLoading(true);
     fetch("/api/event2/consent", {
       method: "POST",
       headers: {
@@ -49,6 +54,7 @@ export default function TermsConditions() {
       .then((res) => res.json())
       .then((data) => {
         window.location.href = "/";
+        setIsLoading(false);
         // location.reload();
       });
   }
@@ -88,6 +94,7 @@ export default function TermsConditions() {
   return (
     <div className="h-full w-full bg-[#0e0e0e]">
       {/* <Image src={bg} alt="bgImage" fill className="object-cover z-[-10] w-full min-h-[100vh]" /> */}
+      {isLoading && <Loader/>}
       <div className="flex flex-col items-center justify-center bg-transparent h-screen max-md:overflow-scroll max-md:max-h-fit ">
         <div className="w-1/2 h-1/2 max-md:h-fit max-md:w-2/3 p-8 flex flex-col justify-around items-center bg-transparent rounded-lg border border-white">
           <p className="text-white text-xl font-bold flex max-md:align-top">
