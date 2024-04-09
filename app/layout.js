e1618329d694bb82540db20fc141de6633db074f
arjunbector <arjunbector@gmail.com>
2024-04-09 12:19:52 +0530

    add favicon

--- app/layout.js
+++ app/layout.js
@@ -9,12 +9,16 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "E-Summit'24",
   description: "Entreprenuership Summit 2024",
+  icons:{
+    icon: "/favicon.ico",
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <AuthProvider>
           <div className="mx-auto text-white">
             <Navbar />
