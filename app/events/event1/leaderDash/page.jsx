2d3709783cc37d0ae99420f91058a051a55623ca
kv8gh <vyaskaran1409@gmail.com>
2024-04-12 10:49:58 +0530

    removing start quiz button

--- app/events/event1/leaderDash/page.jsx
+++ app/events/event1/leaderDash/page.jsx
@@ -239,14 +239,14 @@ export default function LeaderDashboard() {
       </div>
 
       <div className="flex justify-center mt-4">
-        <button
+        {/* <button
           className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           onClick={() => {
             router.push("/events/round0");
           }}
         >
           Start Quiz
-        </button>
+        </button> */}
 
         {/* <DeleteTeamButton onClick={() => deleteTeam()} /> */}
 
